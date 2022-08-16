a8f8c233ffba4bb79be0373341ae82acaeeea622
AKHILACH1 <111353688+AKHILACH1@users.noreply.github.com>
2022-08-16 16:27:10 +0530

    sample programe

---
+++ 01sample/src/HelloSample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloSample {
+	public static void main(String ar[]) {
+  System.out.println("Enter 2 number");
+  Scanner s=new Scanner(System.in);
+  int a=s.nextInt();
+  int b=s.nextInt();
+  
+  int c=a+b;
+  System.out.println("Result:"+c);
+ 
+}}
